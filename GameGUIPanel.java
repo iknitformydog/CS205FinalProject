b37f87b6d76ab70619f5ce14b2c0204b91055dca
Danielle Steimke <danielle@clark.(none)>
2013-03-22 13:20:01 -0400

    GUI shell panel

---
+++ GameGUIPanel.java
@@ -0,0 +1,16 @@
+public class GameGUIPanel{
+
+	Constructor
+		Initialize handPanel
+		initialize handPanel(AI)
+		initialize Deck(Center)Panel
+		initialize ScorePanel
+
+	updateGameInfoPanel
+		get gameINfoFromGame.java
+
+	
+
+
+
+}
